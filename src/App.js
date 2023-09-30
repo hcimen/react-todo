a3fcd2243469fda7adf616e343e20a72eea8e3fe
hcimen <haruncimen@gmail.com>
2023-09-30 13:36:35 -0400

    react page running no rotating React Logo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        <p>
+          <h1> Happy Reacting! </h1>
+          <h1> I could create the first page </h1>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
