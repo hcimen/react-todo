676ae52763c25a2e323362d9e1c12ae03abe9071
hcimen <haruncimen@gmail.com>
2023-10-10 09:33:01 -0400

    first step of second assigment

--- src/App.js
+++ src/App.js
@@ -1,53 +1,60 @@
 import React from 'react';
 
-function App() {
-  const todo_list = [
-    {
-      todo: 'Buy groceries',
-      due_date: '2023-10-15',
-      id:1,
-    },
-    {
-      todo: 'Finish project report',
-      due_date: '2023-10-20',
-      id:2,
-    },
-    {
-      todo: 'Go to the gym',
-      due_date: '2023-10-17',
-      id:3,
-    },
-    {
-      todo: 'Read a book',
-      due_date: '2023-10-25',
-      id:4,
-    },
-    {
-      todo: 'Pay bills',
-      due_date: '2023-10-12',
-      id:5,
-    },
-    {
-      todo: 'Plan weekend trip',
-      due_date: '2023-10-30',
-      id:6,
-    },
-  ];
-  return (
+const todo_list = [
+  {
+    todo: 'Buy groceries',
+    due_date: '2023-10-15',
+    id:1,
+  },
+  {
+    todo: 'Finish project report',
+    due_date: '2023-10-20',
+    id:2,
+  },
+  {
+    todo: 'Go to the gym',
+    due_date: '2023-10-17',
+    id:3,
+  },
+  {
+    todo: 'Read a book',
+    due_date: '2023-10-25',
+    id:4,
+  },
+  {
+    todo: 'Pay bills',
+    due_date: '2023-10-12',
+    id:5,
+  },
+  {
+    todo: 'Plan weekend trip',
+    due_date: '2023-10-30',
+    id:6,
+  },
+];
+
+const secret = 'Hello CTD';
+
+const App = () => {
+
     <div>
           <h1> Todo List </h1>
       <ul>
-        {todo_list.map(function(item){
-          return(
+        {todo_list.map((item) => (
           <li key={item.id}> 
-            {item.todo} - {item.due_date} </li>
-        )})}
+           {item.id}. {item.todo} - {item.due_date} </li>
+        ))}
       </ul>
 
       <hr />
+
+            <div>
+              {secret}
+            </div>
+      
+      <hr />
+
     </div>
-    
-  );
 }
 
 export default App;
