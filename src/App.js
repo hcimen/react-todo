a9c9b88c4860e499f7adc8fc69d2598e8c8fa0b2
hcimen <haruncimen@gmail.com>
2023-10-10 09:42:11 -0400

    first part of assignment

--- src/App.js
+++ src/App.js
@@ -1,60 +1,28 @@
 import React from 'react';
-
-const todo_list = [
-  {
-    todo: 'Buy groceries',
-    due_date: '2023-10-15',
-    id:1,
-  },
-  {
-    todo: 'Finish project report',
-    due_date: '2023-10-20',
-    id:2,
-  },
-  {
-    todo: 'Go to the gym',
-    due_date: '2023-10-17',
-    id:3,
-  },
-  {
-    todo: 'Read a book',
-    due_date: '2023-10-25',
-    id:4,
-  },
-  {
-    todo: 'Pay bills',
-    due_date: '2023-10-12',
-    id:5,
-  },
-  {
-    todo: 'Plan weekend trip',
-    due_date: '2023-10-30',
-    id:6,
-  },
-];
+import TodoList from './TodoList';
 
 const secret = 'Hello CTD';
 
 const App = () => {
+  
+  return(
+  
+  <div>
 
-    <div>
-          <h1> Todo List </h1>
-      <ul>
-        {todo_list.map((item) => (
-          <li key={item.id}> 
-           {item.id}. {item.todo} - {item.due_date} </li>
-        ))}
-      </ul>
+      <h1> Todo List </h1>
+
+      <TodoList />
 
       <hr />
 
-            <div>
-              {secret}
-            </div>
+        <div>
+          {secret}
+        </div>
       
       <hr />
 
-    </div>
+  </div>
+  )
 }
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
+const todo_list = [
+    {
+      todo: 'Buy groceries',
+      due_date: '2023-10-15',
+      id:1,
+    },
+    {
+      todo: 'Finish project report',
+      due_date: '2023-10-20',
+      id:2,
+    },
+    {
+      todo: 'Go to the gym',
+      due_date: '2023-10-17',
+      id:3,
+    },
+    {
+      todo: 'Read a book',
+      due_date: '2023-10-25',
+      id:4,
+    },
+    {
+      todo: 'Pay bills',
+      due_date: '2023-10-12',
+      id:5,
+    },
+    {
+      todo: 'Plan weekend trip',
+      due_date: '2023-10-30',
+      id:6,
+    },
+  ];
+
 function TodoList() {
     
-    return
+    return (
+        <ul>
+        {todo_list.map((item) => (
+          <li key={item.id}> 
+           {item.id}. {item.todo} - {item.due_date} </li>
+        ))}
+      </ul>
+    )
 
 }
 
-export default App;
+export default TodoList;
